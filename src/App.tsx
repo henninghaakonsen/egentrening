d3159c4c6eff8313128963806baeda22f296ff2f
Henning Håkonsen <henninghaakonsen@me.com>
2020-11-23 19:40:10 +0100

    Feature/firebase auth (#1)
    * Satt opp firebase auth.
    
    * Sett title
    
    * Legger til github workflow for automatisk deploy
    
    * Fjerner manifest.json
    
    * Oppdater hosting config.
    
    * Legger til chakra ui og logg inn via knapp.
    
    * Legger på minimalt med styling.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
+import {
+    Button,
+    Center,
+    ColorModeProvider,
+    Heading,
+    Text,
+    theme,
+    ThemeProvider,
+} from '@chakra-ui/react';
 import React from 'react';
+import { useFirebase } from './utils/firebase';
 
-function App() {
-    return <div className="App">Hello world</div>;
-}
+const App: React.FC = () => {
+    const { authenticated, loggInn, user } = useFirebase();
+
+    return (
+        <ThemeProvider theme={theme}>
+            <ColorModeProvider
+                options={{
+                    initialColorMode: 'light',
+                }}
+            >
+                <Center flexDir={'column'}>
+                    <Heading>Egentrening</Heading>
+                    {authenticated ? (
+                        <Text>{user?.displayName}</Text>
+                    ) : (
+                        <Button onClick={loggInn}>Logg inn</Button>
+                    )}
+                </Center>
+            </ColorModeProvider>
+        </ThemeProvider>
+    );
+};
 
 export default App;
